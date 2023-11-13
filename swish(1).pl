81224c2999df093d26908fa67e699c551f939613
Luanpaacheco <127217366+Luanpaacheco@users.noreply.github.com>
2023-11-13 16:54:42 -0300

    Update swish(1).pl

--- swish(1).pl
+++ swish(1).pl
@@ -1,65 +1,15 @@
-avo(X,Y) :- pai(X,Z), pai(Z,Y).
-
-irmao(X,Y) :- pai(Z,X), pai(Z,Y), X \= Y.
-irmao(X,Y) :- mae(Z,X), mae(Z,Y), X \= Y.
-
-casas_relacionadas(Z,Y) :- casa(W,Y), casa(X,Z),casado(X,W), Z\=Y.
 
 
+irmao(X, Y) :- pai(Z, X), pai(Z, Y), X \= Y.
+irmao(X, Y) :- mae(Z, X), mae(Z, Y), X \= Y.
 
-ancestral(X,Y) :- pai(X,Y).
-ancestral(X,Y) :- pai(X,Z), ancestral(Z,Y).
-
-
-
-dobro(X,Y) :- Y is X * 2.
+casas_relacionadas(Z, Y) :- casa(W, Y), casa(X, Z), casados(X, W);casados(W, X), Z\=Y, W\=X.
 
 
-casa(X,Y).
-X = doran,
-Y = house martell;
-
-casa(X,Y).
-X = elia,
-Y = house martell;
-
-casa(doran,house martell).
-casa(trystane,house martell).
-casa(oberyn,house martell)
-casa(sand snakes,house martell)
-casa(elia,house martell)
-local(doran, dorne)
-local(trystane, dorne)
-local(oberyn, dorne)
-local(sand snakes, dorne)
-local(elia, dorne)
-pai(doran,trystane)
-pai(oberyn, sand snakes)
-mae(elia,raenys )
-mae(elia,aegon )
-casados(elia,rhaegar)
-
-
-
-
-casa(olenna,house tyrell)
-casa(luthor,house tyrell)
-casa(mace,house tyrell)
-casa(loras,house tyrell)
-casa(margaery,house tyrell)
-local(olenna, the reach)
-local(luthor, the reach)
-local(mace, the reach)
-local(loras, the reach)
-local(margaery, the reach)
-pai(luthor,mace)
-pai(mace,loras)
-pai(mace,margaery)
-mae(olenna,mace)
-casados(olenna,luthor)
-casados(margaery,joffrey)
-casados(margaery,tommen)
-casados(margaery,renly)
+descendentes(Y, X) :- pai(X, Y).
+descendentes(Y, X) :- mae(X, Y).
+descendentes(Y, X) :- pai(X, Z), descendentes(Y, Z).
+descendentes(Y, X) :- mae(X, Z), descendentes(Y, Z).
 
 
 
@@ -67,159 +17,171 @@ casados(margaery,renly)
 
 
 
-
-casa(tywin, house lannister)
-casa(joanna, house lannister)
-casa(jaime, house lannister)
-casa(cersei, house lannister)
-casa(tyrion, house lannister)
-local(tywin,westerland)
-local(joanna,westerland)
-local(jaime, westerland)
-local(cersei,westerland)
-local(tyrion,westerland)
-pai(tywin, jaime)
-pai(tywin, cersei)
-pai(tywin, tyrion)
-mae(joanna, jaime)
-mae(joanna, cersei)
-mae(joanna, tyrion)
-casados(tywin,joanna)
-casados(cersei,robert)
-
-
-
-
-casa(ormund, house baratheon)
-casa(steffon, house baratheon)
-casa(robert, house baratheon)
-casa(stannis, house baratheon)
-casa(renly, house baratheon)
-casa(shireen, house baratheon)
-casa(joffrey, house baratheon)
-casa(myrcella, house baratheon)
-casa(tommen, house baratheon)
-local(ormund, stromlands)
-local(steffon, stromlands)
-local(robert, stromlands)
-local(stannis, stromlands)
-local(renly, stromlands)
-local(shireen, stromlands)
-local(joffrey, stromlands)
-local(myrcella, stromlands)
-local(tommen, stromlands)
-pai(ormund,steffon)
-pai(steffon,robert )
-pai(steffon, stannis)
-pai(steffon, renly)
-pai(robert,joffrey)
-pai(robert,myrcella)
-pai(robert,tommen)
-pai(jaime,joffrey)
-pai(jaime,myrcella)
-pai(jaime,tommen)
-pai(stannis, shireen)
-mae(cersei ,joffrey)
-mae(cersei ,myrcella)
-mae(cersei ,tommen)
-casados(ormund,rhaelle)
+casa(doran, house_martell).
+casa(trystane, house_martell).
+casa(oberyn, house_martell).
+casa(sand_snakes, house_martell).
+casa(elia, house_martell).
+casa(olenna, house_tyrell).
+casa(luthor, house_tyrell).
+casa(mace, house_tyrell).
+casa(loras, house_tyrell).
+casa(margaery, house_tyrell).
+casa(tywin, house_lannister).
+casa(joanna, house_lannister).
+casa(jaime, house_lannister).
+casa(cersei, house_lannister).
+casa(tyrion, house_lannister).
+casa(ormund, house_baratheon).
+casa(steffon, house_baratheon).
+casa(robert, house_baratheon).
+casa(stannis, house_baratheon).
+casa(renly, house_baratheon).
+casa(shireen, house_baratheon).
+casa(joffrey, house_baratheon).
+casa(myrcella, house_baratheon).
+casa(tommen, house_baratheon).
+casa(hoster, house_tully).
+casa(lysa, house_tully).
+casa(edmure, house_tully).
+casa(catelyn, house_tully).
+casa(jon, house_arryn).
+casa(robin, house_arryn).
+casa(rickard, house_stark).
+casa(brandon1, house_stark).
+casa(eddard, house_stark).
+casa(benjen, house_stark).
+casa(lyanna, house_stark).
+casa(robb, house_stark).
+casa(sansa, house_stark).
+casa(arya, house_stark).
+casa(rickon, house_stark).
+casa(brandon2, house_stark).
+casa(aegon_i, house_targaryen).
+casa(maekar_i, house_targaryen).
+casa(aegon_v, house_targaryen).
+casa(maester_aemon, house_targaryen).
+casa(jaehaerys_ii, house_targaryen).
+casa(rhaelle, house_targaryen).
+casa(aerys_ii, house_targaryen).
+casa(rhaegar, house_targaryen).
+casa(viserys, house_targaryen).
+casa(jon_snow, house_targaryen).
+casa(deanerys, house_targaryen).
 
 
 
-
-casa(jon,house arryn )
-casa(robin,house arryn )
-local(jon,the vale )
-local(robin, the vale)
-pai(jon,robin)
-
-
-
-casa(hoster,house tully)
-casa(lysa,house tully)
-casa(edmure,house tully)
-casa(catelyn,house tully)
-local(hoster,the riverlands)
-local(lysa,the riverlands)
-local(edmure,the riverlands)
-local(catelyn,the riverlands)
-pai(hoster, edmure)
-pai(hoster, lysa)
-pai(hoster, catelyn)
-mae(lysa,robin)
-mae(catelyn ,rickon)
-mae(catelyn ,brandon)
-mae(catelyn ,arya)
-mae(catelyn ,sansa)
-mae(catelyn ,robb)
-casados(edmure,roslin frey)
-casados(lysa,jon)
-casados(lysa,little-finger)
-casados(catelyn,eddard)
-
-
-
-casa(rickard,house stark)
-casa(brandon1,house stark)
-casa(eddard,house stark)
-casa(benjen,house stark)
-casa(lyanna,house stark)
-casa(robb,house stark)
-casa(sansa,house stark)
-casa(arya,house stark)
-casa(rickon,house stark)
-casa(brandon2,house stark)
-local(rickard,the north)
-local(brandon1,the north)
-local(eddard,the north)
-local(benjen,the north)
-local(lyanna,the north)
-local(robb,the north)
-local(sansa,the north)
-local(arya,the north)
-local(rickon,the north)
-local(brandon2,the north)
-pai(rickard, brandon1)
-pai(rickard, eddard)
-pai(rickard, benjen)
-pai(rickard, lyanna)
-pai(eddard,robb)
-pai(eddard,sansa)
-pai(eddard,arya)
-pai(eddard,rickon)
-pai(eddard,brandon2)
-mae(lyanna, jon snow)
-casados(sansa,ramsay bolton)
-casados(robb,talisa)
+local(doran, dorne).
+local(trystane, dorne).
+local(oberyn, dorne).
+local(sand_snakes, dorne).
+local(elia, dorne).
+local(olenna, the_reach).
+local(luthor, the_reach).
+local(mace, the_reach).
+local(loras, the_reach).
+local(margaery, the_reach).
+local(ormund, stromlands).
+local(steffon, stromlands).
+local(robert, stromlands).
+local(stannis, stromlands).
+local(renly, stromlands).
+local(shireen, stromlands).
+local(joffrey, stromlands).
+local(myrcella, stromlands).
+local(tommen, stromlands).
+local(tywin, westerland).
+local(joanna, westerland).
+local(jaime, westerland).
+local(cersei, westerland).
+local(tyrion, westerland).
+local(jon,the_vale).
+local(robin, the_vale).
+local(hoster, the_riverlands).
+local(lysa, the_riverlands).
+local(edmure, the_riverlands).
+local(catelyn, the_riverlands).
+local(rickard, the_north).
+local(brandon1, the_north).
+local(eddard, the_north).
+local(benjen, the_north).
+local(lyanna, the_north).
+local(robb, the_north).
+local(sansa, the_north).
+local(arya, the_north).
+local(rickon, the_north).
+local(brandon2, the_north).
 
 
+pai(doran, trystane).
+pai(oberyn, sand_snakes).
+pai(luthor, mace).
+pai(mace, loras).
+pai(mace, margaery).
+pai(tywin, jaime).
+pai(tywin, cersei).
+pai(tywin, tyrion).
+pai(jon, robin).
+pai(hoster, edmure).
+pai(hoster, lysa).
+pai(hoster, catelyn).
+pai(rickard, brandon1).
+pai(rickard, eddard).
+pai(rickard, benjen).
+pai(rickard, lyanna).
+pai(eddard, robb).
+pai(eddard, sansa).
+pai(eddard, arya).
+pai(eddard, rickon).
+pai(eddard, brandon2).
+pai(aegon_i, maekar_i).
+pai(maekar_i, maester_aemon).
+pai(maekar_i, aegon_v).
+pai(aegon_v, jaehaerys_ii).
+pai(aegon_v, rhaelle).
+pai(aerys_ii, rhaegar).
+pai(aerys_ii, viserys).
+pai(aerys_ii, deanerys).
+pai(rhaegar, jon_snow).
+pai(rhaegar, raenys).
+pai(rhaegar, aegon).
+pai(walder_frey, roslin_frey).
+pai(roose_bolton, ramsey_bolton).
+pai(balon_greyjoy, theon_greyjoy).
 
-casa(aegon i,house targaryen)
-casa(maekar i ,house targaryen)
-casa(aegon v ,house targaryen)
-casa(maester aemon,house targaryen)
-casa(jaehaerys ii,house targaryen)
-casa(rhaelle,house targaryen)
-casa(aerys ii,house targaryen)
-casa(rhaegar,house targaryen)
-casa(viserys,house targaryen)
-casa(jon snow,house targaryen)
-casa(deanerys,house targaryen)
-pai(aegon i, maekar i)
-pai(maekar i , maester aemon)
-pai(maekar i , aegon v)
-pai(aegon v ,jaehaerys ii )
-pai(aegon v ,rhaelle )
-pai(aerys ii, rhaegar)
-pai(aerys ii, viserys)
-pai(aerys ii, deanerys)
-pai(rhaegar, jon snow)
-pai(rhaegar, raenys)
-pai(rhaegar, aegon)
-mae(rhaelle, steffon)
-casados(rhaegar,lyanna)
+mae(elia, raenys).
+mae(elia, aegon).
+mae(olenna, mace).
+mae(joanna, jaime).
+mae(joanna, cersei).
+mae(joanna, tyrion).
+mae(cersei ,joffrey).
+mae(cersei ,myrcella).
+mae(cersei ,tommen).
+mae(lysa, robin).
+mae(catelyn, rickon).
+mae(catelyn, brandon).
+mae(catelyn, arya).
+mae(catelyn, sansa).
+mae(catelyn, robb).
+mae(lyanna, jon_snow).
+mae(rhaelle, steffon).
 
 
-pai(walder frey,roslin frey).
-pai(roose bolton,ramsey bolton).
-pai(balon greyjoy,theon greyjoy).
+casados(elia, rhaegar).
+casados(olenna, luthor).
+casados(margaery, joffrey).
+casados(margaery, tommen).
+casados(margaery, renly).
+casados(tywin, joanna).
+casados(cersei, robert).
+casados(ormund, rhaelle).
+casados(edmure, roslin_frey).
+casados(lysa, jon).
+casados(lysa, little-finger).
+casados(catelyn, eddard).
+casados(sansa, ramsay_bolton).
+casados(robb, talisa).
+casados(rhaegar, lyanna).
+
+
